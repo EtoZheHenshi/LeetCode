094fa03aab37bf0047fec761b515041137683b8e
EtoZheHenshi <henshiwork@gmail.com>
2025-02-12 22:29:23 +0300

    Add 9

---
+++ LeetCode/Easy/9/Solution.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+/* 9. Palindrome Number
+link - https://leetcode.com/problems/palindrome-number/description/
+
+Given an integer x, return true if x is a palindrome, and false otherwise.
+*/
+
+public class Solution_1
+{
+    public bool IsPalindrome(int x)
+    {
+        if (x < 0)
+        {
+            return false;
+        }
+
+        int num = x;
+        int revNum = 0;
+        while (x > 0)
+        {
+            int remainder = x % 10;
+            revNum = revNum * 10 + remainder;
+            x /= 10;
+        }
+        if (num == revNum)
+        {
+            return true;
+        }
+        return false;
+    }
+}
+
+public class Solution_2
+{
+    public bool IsPalindrome(int x)
+    {
+        string num = Convert.ToString(x);
+        char[] revNum = new char[num.Length];
+        for (int i = 0; i < revNum.Length ; i++)
+        {
+            revNum[i] = num[revNum.Length - 1 - i];
+        }
+        if (new string(revNum) == num)
+        {
+            return true;
+        }
+        return false;
+    }
+}
--- README.md
+++ README.md
@@ -12,5 +12,6 @@ My solved problems from [LeetCode](https://leetcode.com/problems/)
   | # | Title | Solution |   Date   | Runtime | Memory |
   |---| ----- | -------- | -------- | --------| ------ |
   | 1 | [Two Sum](https://leetcode.com/problems/two-sum/) | [C#](./LeetCode/Easy/1/Solution.cs) | 11.02.2025 | [Runtime]()|[Memory]()
+  | 9 | [Palindrome Number](https://leetcode.com/problems/palindrome-number/) | [C#](./LeetCode/Easy/9/Solution.cs) | 12.02.2025 | [Runtime]()|[Memory]()
 
 </details>
