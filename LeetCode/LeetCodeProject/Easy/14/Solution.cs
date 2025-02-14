61c19febe3885d6cb267d689c84f069f074bbe7e
EtoZheHenshi <henshiwork@gmail.com>
2025-02-14 14:58:18 +0300

    Add 14

---
+++ LeetCode/LeetCodeProject/Easy/14/Solution.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/* 14. Longest Common Prefix
+link - https://leetcode.com/problems/longest-common-prefix/
+
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+ 
+
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+ 
+
+Constraints:
+
+1 <= strs.length <= 200
+0 <= strs[i].length <= 200
+strs[i] consists of only lowercase English letters if it is non-empty.
+*/
+
+public class Solution_14
+{
+    public string LongestCommonPrefix(string[] strs)
+    {
+        int index = 0;
+        StringBuilder pref = new StringBuilder("");
+        string firstWord = strs[0];
+        bool check = true;
+        while (check)
+        {
+            if (firstWord.Length > index)
+            {
+                pref.Append(firstWord[index++]);
+                foreach (string str in strs)
+                {
+                    if (!str.StartsWith(pref.ToString()))
+                    {
+                        pref.Remove(pref.Length - 1, 1);
+                        check = false;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                check = false;
+            }
+        }
+        return pref.ToString();
+    }
+}
--- README.md
+++ README.md
@@ -14,5 +14,6 @@ My solved problems from [LeetCode](https://leetcode.com/problems/)
   | 1 | [Two Sum](https://leetcode.com/problems/two-sum/) | [C#](./LeetCode/LeetCodeProject/Easy/01/Solution.cs) | 11.02.2025 | [Runtime]()|[Memory]()
   | 9 | [Palindrome Number](https://leetcode.com/problems/palindrome-number/) | [C#](./LeetCode/LeetCodeProject/Easy/09/Solution.cs) | 12.02.2025 | [Runtime]()|[Memory]()
   | 13 | [Roman to Integer](https://leetcode.com/problems/roman-to-integer/) | [C#](./LeetCode/LeetCodeProject/Easy/13/Solution.cs) | 13.02.2025 | [Runtime]()|[Memory]()
+  | 14 | [Longest Common Prefix](https://leetcode.com/problems/longest-common-prefix/) | [C#](./LeetCode/LeetCodeProject/Easy/14/Solution.cs) | 14.02.2025 | [Runtime]()|[Memory]()
 
 </details>
